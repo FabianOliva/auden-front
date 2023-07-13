cc4bbe149213f182493d44adfe47df067bd5fde1
FabianOliva <fabianoliva01@hotmail.com>
2023-07-13 20:33:38 -0300

    start front

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./index.css";
+import { logo } from "../public/logo-large.png";
 
 function App() {
   return (
--- src/assets/components/Default_btn/index.jsx
+++ src/assets/components/Default_btn/index.jsx
@@ -1,5 +1,24 @@
-const Default_btn = () => {
-    return (  );
-}
- 
-export default Default-btn;
+export const Default_btn_Active = (props) => {
+  return (
+    <a href="" className="Default_btn-orange">
+      {props.text}
+    </a>
+  );
+};
+
+export const Default_btn_login = (props) => {
+  return (
+    <a href="" className="Default_btn-orange">
+      <img src={props.img} alt="" />
+      {props.text}
+    </a>
+  );
+};
+
+export const Default_btn_Inactive = (props) => {
+  return (
+    <a href="" className="Default_btn-orange">
+      {props.text}
+    </a>
+  );
+};
