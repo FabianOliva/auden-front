e393019efb120f7464a161a64aedae83ccfa0142
FabianOliva <fabianoliva01@hotmail.com>
2023-07-13 19:40:43 -0300

    startup

---
+++ src/assets/Pages/start-up/index.jsx
@@ -0,0 +1,14 @@
+import { logo } from "../../../../public/logo-large.png";
+
+const Start = () => {
+  return (
+    <>
+      <section>
+        <img src={logo} alt="logo" />
+        <h1></h1>
+      </section>
+    </>
+  );
+};
+
+export default Start;
