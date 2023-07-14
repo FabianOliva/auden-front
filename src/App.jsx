b76d2ef0fea144f610b452220db1611382007c4e
FabianOliva <fabianoliva01@hotmail.com>
2023-07-13 22:37:42 -0300

    front of start page done

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,33 @@
-import { useState } from "react";
 import "./App.css";
-import Home from "./assets/Pages/home";
+import { useState } from "react";
 import * as React from "react";
 import * as ReactDOM from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import "./index.css";
-import logo from "../public/logo-large.png";
+import logo from "../src/public/logo-large.png";
+import { Default_btn_Active, Default_btn_login, Default_btn_Inactive } from "./assets/components/Default_btn";
+import google from "../src/public/google-icon.png";
+import apple from "../src/public/apple-icon.png";
 
 function App() {
   return (
     <>
       <section className="start-logo-sect">
-        <div className="start-logo-sect">
+        <div className="start-logo-cont">
           <img src={logo} alt="logo" className="start-logo-img" />
           <h1 className="start-logo-h1">Música a medida</h1>
         </div>
       </section>
-
-      <section>
-        <div></div>
+      <section className="start-btns-sect">
+        <div className="start-btns-cont">
+          <Default_btn_Active text="Registrarse Gratis" />
+          <Default_btn_login text="Continuar con Google" img={google} />
+          <Default_btn_login text="Continuar con Apple" img={apple} />
+        </div>
+        <div className="start-login">
+          <a className="start-login-text" href="">
+            Iniciar Sesion
+          </a>
+        </div>
       </section>
     </>
   );
