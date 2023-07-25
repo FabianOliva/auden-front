c2c1fa0a4f562468eef3325d8a47d757e18f327e
FabianOliva <fabianoliva01@hotmail.com>
2023-07-25 01:29:17 -0300

    Css Fixes

--- src/assets/Pages/home/index.jsx
+++ src/assets/Pages/home/index.jsx
@@ -9,6 +9,8 @@ import { NavLink } from "react-router-dom";
 const Home = () => {
   const [animationStep, setAnimationStep] = useState(1);
   const [animationStep2, setAnimationStep2] = useState(1);
+  const [hover, setHover] = useState(false);
+  const [hover2, setHover2] = useState(false);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -34,13 +36,41 @@ const Home = () => {
     return () => clearInterval(interval);
   }, [animationStep2]);
 
+  const handleMouseEnter = () => {
+    setHover(true);
+  };
+
+  const handleMouseLeave = () => {
+    setHover(false);
+  };
+
+  const handleMouseEnter2 = () => {
+    setHover2(true);
+  };
+
+  const handleMouseLeave2 = () => {
+    setHover2(false);
+  };
+
   return (
     <div className="home-main-cont page-container">
       <header className="home-header">
         <h1 className="home-header-title">Música ya</h1>
         <div className="home-header-icons">
-          <img src={history} alt="" className="home-header-icon" />
-          <img src={bell} alt="" className="home-header-icon" />
+          <img
+            src={history}
+            alt=""
+            className={hover ? "home-header-icon rotate-center" : "home-header-icon"}
+            onMouseEnter={handleMouseEnter}
+            onMouseLeave={handleMouseLeave}
+          />
+          <img
+            src={bell}
+            alt=""
+            className={hover2 ? "home-header-icon wobble-hor-top" : "home-header-icon"}
+            onMouseEnter={handleMouseEnter2}
+            onMouseLeave={handleMouseLeave2}
+          />
         </div>
       </header>
       <main className="home-main">
@@ -49,9 +79,7 @@ const Home = () => {
           <NavLink className="link_card" to="/cupido-musical">
             <div className="main-card-text">
               <p className="main-card-text-title">Cupido Musical</p>
-              <p className="main-card-text-text">
-                Tus artistas favoritos nunca van a dejarte con el corazón roto.
-              </p>
+              <p className="main-card-text-text">Tus artistas favoritos nunca van a dejarte con el corazón roto.</p>
             </div>
           </NavLink>
         </div>
@@ -61,21 +89,13 @@ const Home = () => {
           <NavLink to="/musica-contextual">
             <div className="main-card-text">
               <p className="main-card-text-title">Música Contextual</p>
-              <p className="main-card-text-text">
-                {" "}
-                Creamos la playlist perfecta para cualquier situación.
-              </p>
+              <p className="main-card-text-text"> Creamos la playlist perfecta para cualquier situación.</p>
             </div>{" "}
           </NavLink>
         </div>
       </main>
 
-      <Nav_Bar
-        homeOpacity="100%"
-        searchOpacity="50%"
-        accountOpacity="50%"
-        friendsOpacity="50%"
-      />
+      <Nav_Bar homeOpacity="100%" searchOpacity="50%" accountOpacity="50%" friendsOpacity="50%" />
     </div>
   );
 };
