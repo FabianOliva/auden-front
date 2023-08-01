7014e227a8a5859090123eca2894dc7b8ed4b05d
Fabian Oliva <130715592+FabianOliva@users.noreply.github.com>
2023-08-01 20:28:14 -0300

    Revert "Cambios actuales"

--- src/assets/Pages/log-in/index.jsx
+++ src/assets/Pages/log-in/index.jsx
@@ -1,20 +1,151 @@
-
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./index.css";
+import cookies from "js-cookie";
 
 export const Login = () => {
-  const [password, setPassword] = useState("");
-  const [showPassword, setShowPassword] = useState(false);
-  const handlePasswordChange = (event) => {
-    setPassword(event.target.value);
+  //Diseño
+  // const [password, setPassword] = useState("");
+  // const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState(null);
+
+  // const handlePasswordChange = (event) => {
+  //   setPassword(event.target.value);
+  // };
+  // const toggleShowPassword = () => {
+  //   setShowPassword(!showPassword);
+  // };
+  //---------------USER-VALIDATOR---------------/
+
+  const [fetchDatUser, setFetchDataUser] = useState([]);
+
+  const fetchDataUser = async () => {
+    try {
+      const response = await fetch(`http://localhost:3002/users`);
+      const dataUsers = await response.json();
+      setFetchDataUser(dataUsers);
+      console.log(dataUsers);
+      //setLoading(false);
+    } catch (error) {
+      console.log("fallo al traer usuarios", error);
+      //setLoading(false);
+    }
   };
-  const toggleShowPassword = () => {
-    setShowPassword(!showPassword);
+
+  useEffect(() => {
+    fetchDataUser();
+  }, []);
+
+  const login = async () => {
+    let headersList = {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    };
+
+    let bodyContent = JSON.stringify({
+      user_username: formData.nombreoemail,
+      user_userpassword: formData.password,
+    });
+
+    let response = await fetch("http://localhost:3002/users/login", {
+      method: "POST",
+      body: bodyContent,
+      headers: headersList,
+    });
+
+    if (response.ok) {
+      let data = await response.json();
+      const token = data.token; // Obtener el token de la respuesta
+      cookies.set("userToken", token, { expires: 1 });
+      console.log(data);
+
+      // Redirigir al usuario a la siguiente página después del inicio de sesión exitoso
+      window.location.href = "http://localhost:5173/home";
+    } else {
+      // Si el inicio de sesión falla, puedes mostrar un mensaje de error
+      console.log("Inicio de sesión fallido");
+      let errorResponse = await response.json();
+      const errorMessage = errorResponse.mensaje;
+      setError(errorMessage);
+    }
   };
+  //--------------------------VALIDAR-----------------------//
+
+  const [formData, setFormData] = useState({
+    nombreoemail: "",
+    password: "",
+  });
+
+  const handleChange = (event) => {
+    const { name, value } = event.target;
+    setFormData({ ...formData, [name]: value });
+
+    // Reiniciar el campo en emptyFields cuando cambie el contenido del campo
+    setEmptyFields((prevEmptyFields) => ({
+      ...prevEmptyFields,
+      [name]: false,
+    }));
+  };
+
+  //        Validacion de inputs en Front
+
+  const [hasErrors, setHasErrors] = useState(false);
+
+  const [formErrors, setFormErrors] = useState({
+    nombreoemail: "",
+    password: "",
+  });
+
+  const [emptyFields, setEmptyFields] = useState({
+    nombreoemail: false,
+    password: false,
+  });
+
+  const validateForm = () => {
+    let isValid = true;
+    const errors = {};
+    const emptyFieldsTemp = { ...emptyFields };
+
+    // Validation for nombreoemail field
+    if (!formData.nombreoemail) {
+      errors.nombreoemail = "El nombre de usuario o e-mail es requerido.";
+      emptyFieldsTemp.nombreoemail = true;
+      isValid = false;
+    } else {
+      emptyFieldsTemp.nombreoemail = false;
+    }
+
+    // Validation for password field
+    if (!formData.password) {
+      errors.password = "La contraseña es requerida.";
+      emptyFieldsTemp.password = true;
+      isValid = false;
+    } else {
+      emptyFieldsTemp.password = false;
+    }
+
+    setFormErrors(errors);
+    setEmptyFields(emptyFieldsTemp);
+    setHasErrors(!isValid);
+    return isValid;
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    if (validateForm()) {
+      try {
+        await login();
+      } catch (error) {
+        // Handle any errors that might occur during login
+        console.error("An error occurred during login:", error);
+        // Add code here to display an error message to the user
+      }
+    }
+  };
+
   return (
     <div className="login_container">
       <nav className="navbar_container">
-        <a href="http://localhost:5173/">
+        <a href="/">
           {" "}
           <img src="src\public\left-icon-placeholder.png" alt="<-" />
         </a>
@@ -26,51 +157,49 @@ export const Login = () => {
       <form action="">
         <div className="form_container_login">
           {/* --------------INPUT NAME--------------- */}
-          <div className="labels_input">
-            {/* ADD OK  */}
+          <div className={`labels_input ${error ? "error" : ""}`}>
             <label htmlFor="">
               <p>Nombre de Usuario o E-mail:</p>
-              <input type="text" />
+              <input
+                type="text"
+                name="nombreoemail"
+                value={formData.nombreoemail}
+                onChange={handleChange}
+                className={emptyFields.nombreoemail ? "InputError " : ""}
+                placeholder={emptyFields.nombreoemail ? "Este campo es requerido." : ""}
+              />
             </label>
           </div>
           {/* -------------INPUT PASSWORD------------- */}
-          <div className="labels_input password ">
-            {/* ADD ERROR or OK  */}
+          <div className="labels_input passwordd">
             <label htmlFor="">
               <p>Contraseña:</p>
-
-              <div className="input_img">
+              <div className={`input_img ${error ? "error" : ""}`}>
                 <input
-                  type={showPassword ? "text" : "password"}
-                  value={password}
-                  onChange={handlePasswordChange}
-                />
-                <img
-                  src={
-                    showPassword
-                      ? "src/public/ojo.png"
-                      : "src/public/ojo-cerrado.png"
-                  }
-                  alt="Toggle password visibility"
-                  onClick={toggleShowPassword}
+                  type="password"
+                  name="password"
+                  value={formData.password}
+                  onChange={handleChange}
+                  className={emptyFields.password ? "InputError" : ""}
+                  placeholder={emptyFields.password ? "Este campo es requerido." : ""}
                 />
               </div>
-
             </label>
+            {error && <div className="errorlogin">Credenciales Inválidas</div>}
           </div>
           {/*----------- BUTTON------------- */}
           <div className="footer">
-
             <a
-              href="http://localhost:5173/home"
-              className="Default_btn standar"
-            >
+              onClick={handleSubmit}
+              className={`Default_btn ${
+                hasErrors || !formData.nombreoemail || !formData.password ? "standar" : "actived"
+              }`}
+              disabled={hasErrors || !formData.nombreoemail || !formData.password}>
               Iniciar Sesión
             </a>
             <a href="http://localhost:5173/recuperarcuenta">
               <p>¿Olvidaste tu contraseña?</p>
             </a>
-
           </div>
         </div>
       </form>
--- src/assets/Pages/sign-up/index.jsx
+++ src/assets/Pages/sign-up/index.jsx
@@ -1,8 +1,35 @@
 import React from "react";
 import "./index.css";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const Register = () => {
+  const [email, setEmail] = useState("");
+  const [error, setError] = useState("");
+
+  const handleChange = (event) => {
+    setEmail(event.target.value);
+    setError("");
+  };
+
+  // Obtener el objeto de navegación
+  const navigate = useNavigate();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (email.trim() === "") {
+      setError("El campo de correo electrónico no puede estar vacío.");
+    } else if (!emailRegex.test(email)) {
+      setError("El correo electrónico ingresado no es válido.");
+    } else {
+      setError("");
+      window.location.href = `/Registeruser?email=${encodeURIComponent(email)}`;
+    }
+  };
+
   return (
     <div className="login_container">
       <nav className="navbar_container">
@@ -12,21 +39,34 @@ export const Register = () => {
         <p>Crear Cuenta</p>
         <div className="relleno"></div>
       </nav>
-      <form action="">
-        <p className="p_title">¿Cuál es tu correo electrónico ?</p>
+      <form className="Register_form">
+        <p className="p_title">¿Cuál es tu correo electrónico?</p>
         <div className="form_container_login">
           <div className="labels_input register">
-            <label htmlFor="">
+            <label htmlFor="email">
               <p>Correo electrónico:</p>
-              <input type="text" />
+              <input
+                type="text"
+                id="email"
+                value={email}
+                onChange={handleChange}
+                placeholder={error ? "El campo no debe estar vacío" : ""}
+                className={error ? "InputError" : ""}
+              />
               <div className="footer register">
-                <p>Deberas poder confirmarlo luego</p>
+                <p>Deberás poder confirmarlo luego</p>
               </div>
             </label>
           </div>
-          <a className="Default_btn active register" href="http://localhost:5173/registeruser">
+          <Link
+            to={{
+              pathname: "/Registeruser",
+              state: { email },
+            }}
+            className={`Default_btn actived register ${error ? "disabled" : ""}`}
+            onClick={handleSubmit}>
             Continuar
-          </a>
+          </Link>
         </div>
       </form>
     </div>
